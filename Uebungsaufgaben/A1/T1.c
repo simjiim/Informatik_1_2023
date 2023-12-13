6b8675b7873bd828eddc619f16a1c563bee1b3a3
Simeon Stuermer <sist00002@stud.uni-saarland.de>
2023-12-13 20:17:51 +0100

    MOVED: A81

---
+++ Uebungsaufgaben/A1/T1.c
@@ -0,0 +1,31 @@
+//
+// Created by simeon on 11.12.23.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double kontostand=0, zinssatz=0;
+    int jahre=0, i=0;
+
+    printf("Size int: %i\n", sizeof(int));
+    printf("Size long: %i\n", sizeof(long));
+    printf("Size long long: %i\n", sizeof(long long));
+
+
+    printf("Geben Sie den Kontostand ein!\n");
+    scanf("%lf", &kontostand);
+    printf("Geben Sie den Zinssatz p.a. an! (in %%)\n");
+    scanf("%lf", &zinssatz);
+    printf("Wie viele Jahre sollen simuliert werden?\n");
+    scanf("%i", &jahre);
+
+    for(i=0; i<jahre; i++){
+        kontostand*=(1+zinssatz/100);
+        printf("Der Kontostand im Jahr %i: %lf", i+1, kontostand);
+    }
+
+
+
+};
